92d12f2e55fa438e3afaa3c3cfedabd476608f67
mihanizm56 <m727507.56@mail.ru>
2019-11-20 00:38:49 +0200

    fix(fix): fix
    fix

--- src/services/_redux/index.ts
+++ src/services/_redux/index.ts
@@ -1,16 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import { createReducer } from '@/_utils/redux/create-reducer';
 import { rootReducer } from './root-reducer';
 import { IAdvancedStore } from './_types';
 
+// TODO fix redux devtools
 export const createAppStore = () => {
   const sagaMiddleware = createSagaMiddleware();
 
   const store: IAdvancedStore = createStore(
     createReducer({ prevState: rootReducer }),
     applyMiddleware(sagaMiddleware),
+    // composeWithDevTools(applyMiddleware(sagaMiddleware)),
   );
 
   // Add a dictionary to keep track of the registered async reducers sagas
